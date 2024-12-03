ee9158ae86b0278d8e48f53c760a9b4437b83bea
PlantWasTaken <69759382+PlantWasTaken@users.noreply.github.com>
2024-12-03 07:33:17 +0100

    Update adbapi.py
    added better cde

--- adbapi.py
+++ adbapi.py
@@ -1,30 +1,16 @@
 import os
 import subprocess
 from PIL import Image
-import pytesseract
 import re
 
-class Device:
-    def __init__(self, port, _devices, emulator, _name) -> None:
+class BaseDevice:
+    def __init__(self, adb_path=None):
         current_dir = os.getcwd()
-        adb_path = self.find_executable('adb.exe', current_dir)
-        if not adb_path:
+        self.adb = adb_path or self.find_executable('adb.exe', current_dir)
+        if not self.adb:
             raise FileNotFoundError("adb.exe not found in the current directory or subdirectories.")
         
-        self.name = _name #only used if emulator = 1
-        self.emulator = emulator #using emulator 1 - yes, 0 - no
-        self.adb = adb_path
-        self.port = port  # Port of emulator to run code on, default #5554
-        self.devices = _devices  # Number of devices, default 1, only tested for 3..., 0 - checks for devices
-
         subprocess.call(['taskkill', '/F', '/IM', 'adb.exe'])
-        if(self.emulator == 1): #else establish connection to phonep
-            self.connect_emulators()
-
-        if(self.emulator == 0):
-            subprocess.run(f'"{self.adb}" devices', shell=True)
-            subprocess.run(f'"{self.adb}" -s {self.name} shell wm size', check=True)
-        
         subprocess.run(f'"{self.adb}" start-server', shell=True)
 
     def find_executable(self, filename, search_path):
@@ -34,51 +20,67 @@ class Device:
                 return os.path.join(root, filename)
         return None
 
-    def screenshot(self) -> Image:
-        screenshot_path = os.path.join(os.getcwd(), f'emu-{self.port}.png')
-        screenshot_path_phone = os.path.join(os.getcwd(), f'phn-{self.name}.png')
-        print(screenshot_path_phone)
+    def screenshot(self, device_identifier):
+        screenshot_path = os.path.join(os.getcwd(), f'{device_identifier}.png')
         temp_screenshot_path = '/data/local/tmp/image.png'
-
-        if(self.emulator == 1):
-            subprocess.call(f'"{self.adb}" -s emulator-{self.port} shell screencap -p {temp_screenshot_path}', shell=True)
-            subprocess.run(f'"{self.adb}" -s emulator-{self.port} pull {temp_screenshot_path} "{screenshot_path}"', shell=True)
-            screenshot = Image.open(screenshot_path)
-
-        if(self.emulator == 0):
-            subprocess.call(f'"{self.adb}" -s {self.name} shell screencap -p {temp_screenshot_path}', shell=True)
-            subprocess.run(f'"{self.adb}" -s {self.name} pull {temp_screenshot_path} "{screenshot_path_phone}"', shell=True)
-            
-            screenshot = Image.open(screenshot_path_phone)
-
-        return screenshot
-
-    def screenInput(self, x, y) -> None:
-        # Sending a touch input command to the emulator
         
-        if(self.emulator == 1):
-            print(f'Input emulator-{self.port} at: {x}, {y}')
-            subprocess.Popen(f'"{self.adb}" -s emulator-{self.port} shell input tap {x} {y}', shell=True)
-        if(self.emulator == 0):
-            print(f'Input {self.name} at: {x}, {y}')
-            subprocess.Popen(f'"{self.adb}" -s {self.name} shell input tap {x} {y}', shell=True)
+        subprocess.call(f'"{self.adb}" -s {device_identifier} shell screencap -p {temp_screenshot_path}', shell=True)
+        subprocess.run(f'"{self.adb}" -s {device_identifier} pull {temp_screenshot_path} "{screenshot_path}"', shell=True)
+        
+        return Image.open(screenshot_path)
 
-    def screenSwipe(self,x1,y1,x2,y2) -> None:
+    def screenInput(self, device_identifier, x, y):
+        print(f'Input {device_identifier} at: {x}, {y}')
+        subprocess.Popen(f'"{self.adb}" -s {device_identifier} shell input tap {x} {y}', shell=True)
+
+    def screenSwipe(self, device_identifier, x1, y1, x2, y2):
         print(f'Swiping from: {x1}, {y1} -> {x2}, {y2}')
-        if(self.emulator == 1):
-            subprocess.Popen(f'"{self.adb}" -s emulator-{self.port} shell input touchscreen swipe {x1} {y1} {x2} {y2}', shell=True)
-        if(self.emulator == 0):
-            subprocess.Popen(f'"{self.adb}" -s {self.name} shell input touchscreen swipe {x1} {y1} {x2} {y2}', shell=True)
+        subprocess.Popen(f'"{self.adb}" -s {device_identifier} shell input touchscreen swipe {x1} {y1} {x2} {y2}', shell=True)
 
-    @staticmethod
-    def generate_ports(self) -> list: #emulator
+
+class Phone(BaseDevice):
+    def __init__(self, name, adb_path=None):
+        super().__init__(adb_path)
+        self.name = name
+        
+        subprocess.run(f'"{self.adb}" devices', shell=True)
+        subprocess.run(f'"{self.adb}" -s {self.name} shell wm size', check=True)
+
+    def screenshot(self):
+        return super().screenshot(self.name)
+
+    def screenInput(self, x, y):
+        super().screenInput(self.name, x, y)
+
+    def screenSwipe(self, x1, y1, x2, y2):
+        super().screenSwipe(self.name, x1, y1, x2, y2)
+
+
+class Device(BaseDevice):
+    def __init__(self, port, devices, emulator,name=None, adb_path=None):
+        super().__init__(adb_path)
+        self.port = port
+        self.devices = devices
+        self.emulator = emulator
+        self.name = name
+
+
+        if self.emulator:
+            self.connect_emulators()
+        else:
+            subprocess.run(f'"{self.adb}" devices', shell=True)
+            if self.name:
+                subprocess.run(f'"{self.adb}" -s {self.name} shell wm size', check=True)
+
+
+    def generate_ports(self):
         if self.devices == 0:
             print(f'Finding devices..')
             devices_output = subprocess.run(f'"{self.adb}" devices', shell=True, capture_output=True, text=True)
             emulator_ports = re.findall(r'emulator-(\d+)', devices_output.stdout)
             ports = [int(port) for port in emulator_ports]
 
-            if(len(ports) == 0):
+            if not ports:
                 print(f'No adb device detected')
                 raise SystemError
             else:
@@ -86,52 +88,26 @@ class Device:
                 print(f'Found {len(ports)} devices')
                 print(f'Ports: {ports}')
 
-        if self.devices == -1: #if you know the device id
+        if self.devices == -1:
             return [self.port]
-        
-        if self.devices == 1:
-            return [5554]  # Default port
-        if self.devices == 2:
-            return [5554, 5558]
-        if self.devices > 2:
-            return [(5554 + 2 * i) for i in range(self.devices)]
 
-    def connect_emulators(self) -> None:
-        # Connects to all emulators using the generated ports
-        _ports = Device.generate_ports(self)
-        print(f'Ports: {_ports}')
-        for port in _ports:
+        return [(5554 + 2 * i) for i in range(self.devices)]
+
+    def connect_emulators(self):
+        ports = self.generate_ports()
+        print(f'Ports: {ports}')
+        for port in ports:
             subprocess.run(f'"{self.adb}" connect 127.0.0.1:{port}', check=True)
             subprocess.run(f'"{self.adb}" -s emulator-{port} shell wm size', check=True)
 
-class ImageOcr:
-    def __init__(self,im) -> None:
-        # Search for tesseract.exe in the current directory or subdirectories
-        current_dir = os.getcwd()
-        tesseract_path = self.find_executable('tesseract.exe', current_dir)
-        if not tesseract_path:
-            raise FileNotFoundError("tesseract.exe not found in the current directory or subdirectories.")
-        
-        pytesseract.pytesseract.tesseract_cmd = tesseract_path
-        self.im = im 
+    def screenshot(self):
+        identifier = f"emulator-{self.port}" if self.emulator else self.name
+        return super().screenshot(identifier)
 
-    def find_executable(self, filename, search_path):
-        for root, dirs, files in os.walk(search_path):
-            if filename in files:
-                return os.path.join(root, filename)
-        return None
+    def screenInput(self, x, y):
+        identifier = f"emulator-{self.port}" if self.emulator else self.name
+        super().screenInput(identifier, x, y)
 
-    def get_text(self) -> str:
-        # Extracting text from the image using pytesseract
-        text = pytesseract.image_to_string(self.im).split()
-        print(text)
-        return text
-
-# Example usage:
-# a = Device(0, 2)
-# im1 = a.screenshot()
-
-# my_text = ImageOcr(im1)
-# print(my_text.get_text())
-
-# a.screenInput(100, 200)
+    def screenSwipe(self, x1, y1, x2, y2):
+        identifier = f"emulator-{self.port}" if self.emulator else self.name
+        super().screenSwipe(identifier, x1, y1, x2, y2)
